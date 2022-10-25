d01c4726fec98af6d28c954b8371eb3c6ea54b95
Ariel <sitioslaa@gmail.com>
2022-10-25 00:42:53 -0300

    Ejemplo de request en Mercado Libre

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import MercadoLibre from './components/MercadoLibre/MercadoLibre';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <ItemListContainer greeting="Bienvenidos a mi Ecommerce"/>
+      <MercadoLibre />
+
+      {/* <Navbar />
+      <ItemListContainer greeting="Bienvenidos a mi Ecommerce"/> */}
     </div>
   );
 }
